bb487e1e96e8d7173c828d58c9477b2b330757bf
kh214jw <kh214jw@gmail.com>
2023-06-06 15:48:54 +0900

    v0.1 Upload

--- Components/UserRanking/Ranking.js
+++ Components/UserRanking/Ranking.js
@@ -2,37 +2,37 @@ import { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, ScrollView } from 'react-native';
 import Axios from 'axios';
 
-const Ranking = () => {
-  const [data, setData] = useState([]);
-  const [rankType, setRankType] = useState('score');
+const Raking = () => {
+  const [data, setData] = useState([]); //데이터 상태 관리
+  const [rankType, setRankType] = useState('score'); //랭킹 종류 관리
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, []); //fetchdata 함수 호출
 
   const fetchData = async () => {
     try {
       const response = await Axios.get("https://www.smu-enip.site/user/rank?value=weight"); //Replace with your actual API endpoint
-      const fetchedData = response.data;
-      setData(fetchedData);
+      const fetchedData = response.data; //응답데이터 추출
+      setData(fetchedData); //데이터 상태 업데이트
     } catch (error) {
       console.log('Error fetching data:', error);
     }
   };
 
   const updateRankings = () => {
-    const sortedData = [...data].sort((a, b) => b[rankType] - a[rankType]);
+    const sortedData = [...data].sort((a, b) => b[rankType] - a[rankType]); //랭킹 데이터 정렬
     const rankedData = sortedData.map((item, index) => ({
       ...item,
       rank: index + 1,
-    }));
-    setData(rankedData);
+    })); //정렬 순위 추가
+    setData(rankedData); //랭킹데이터 업데이트
   };
 
   const handleRankingToggle = () => {
     const newRankType = rankType === 'score' ? 'weight' : 'score';
-    setRankType(newRankType);
-    updateRankings();
+    setRankType(newRankType); //랭킹 종류 업데이트
+    updateRankings(); //호출
   };
 
   const renderRankingButton = () => {
@@ -53,7 +53,6 @@ const Ranking = () => {
   return (
     <ScrollView style={{ flex: 1 }}>
       <View>
-      
         <View style={styles.tableContainer}>
           <View style={styles.tableHeader}>
             <Text style={styles.columnHeader}>순위</Text>
@@ -63,13 +62,15 @@ const Ranking = () => {
           </View>
 
           {data.map((item, index) => (
-            <View style={styles.tableRow} key={index}>
-              <Text style={styles.column}>{index + 1}</Text>
-              <Text style={styles.column}>{item.nickName}</Text>
-              <Text style={styles.column}>{item.weight}</Text>
-              <Text style={styles.column}>{item.score}</Text>
-            </View>
-          ))}
+  <View style={[styles.tableRow, { backgroundColor: styles.rowColors[index % styles.rowColors.length] }]} key={index}>
+    <Text style={styles.column}>{index + 1}</Text>
+    <Text style={styles.column}>{item.nickName}</Text>
+    <Text style={styles.column}>{item.weight}</Text>
+    <Text style={styles.column}>{item.score}</Text>
+  </View>
+))}
+
+
         </View>
         {renderRankingButton()}
       </View>
@@ -85,48 +86,45 @@ const styles = StyleSheet.create({
     marginTop: 10, 
   },
   button: {
-    backgroundColor: 'green', // 버튼 배경색
+    backgroundColor: 'green',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
   },
   buttonText: {
-    color: 'white', // 버튼 텍스트 색상
+    color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
   },
   tableContainer: {
-    borderWidth: 1,
-    borderColor: 'black',
     backgroundColor: '#c8e6c9',
     borderRadius: 5,
     overflow: 'hidden',
-    marginHorizontal: 15, 
+    marginHorizontal: 15,
   },
   tableHeader: {
     flexDirection: 'row',
-    backgroundColor: '#81c784', // 헤더 배경
+    backgroundColor: '#81c784', 
     paddingVertical: 10,
   },
   columnHeader: {
     flex: 1,
     textAlign: 'center',
     fontWeight: 'bold',
-    color: 'white', // 헤더 텍스트 색상
+    color: 'white', 
     fontSize: 18,
   },
   tableRow: {
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderColor: 'black',
     paddingVertical: 10,
   },
   column: {
     flex: 1,
     textAlign: 'center',
-    color: 'black', // 데이터 텍스트 색상
+    color: 'black',
     paddingVertical: 5,
   },
+  rowColors: ['#f0f8f6', '#e0f2e9'], 
 });
 
-export default Ranking;
+export default Raking;
